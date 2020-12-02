b5da5656f321d36bf0c9cab40ea236255d000c83
Mateusz Nowak <mateusz.nowak@vonage.com>
2020-12-02 21:45:20 +0100

    Test DSL

--- domain/src/test/kotlin/pl/zycienakodach/crimestories/scenarios/mysterydeath/MysteryDeathScenarioTest.kt
+++ domain/src/test/kotlin/pl/zycienakodach/crimestories/scenarios/mysterydeath/MysteryDeathScenarioTest.kt
@@ -12,12 +12,10 @@ import pl.zycienakodach.crimestories.domain.capability.detective.InvestigationSt
 import pl.zycienakodach.crimestories.domain.capability.detective.StartInvestigation
 import pl.zycienakodach.crimestories.domain.capability.item.ItemWasFound
 import pl.zycienakodach.crimestories.domain.capability.location.*
+import pl.zycienakodach.crimestories.domain.policy.investigation.Investigation
 import pl.zycienakodach.crimestories.domain.policy.investigation.SinglePlayerInvestigation
 import pl.zycienakodach.crimestories.domain.policy.investigation.currentTime
-import pl.zycienakodach.crimestories.domain.shared.Command
-import pl.zycienakodach.crimestories.domain.shared.CommandResult
-import pl.zycienakodach.crimestories.domain.shared.DomainEvent
-import pl.zycienakodach.crimestories.domain.shared.DomainEvents
+import pl.zycienakodach.crimestories.domain.shared.*
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.LocalTime
@@ -25,8 +23,6 @@ import java.time.LocalTime
 val detectiveThomas = DetectiveId("Thomas")
 
 class MysteryDeathScenarioTest {
-
-    val investigation = mysteryDeathInvestigation()
 
     @Test
     fun `scenario action starts at Police Station`() {
@@ -49,124 +45,113 @@ class MysteryDeathScenarioTest {
 
     @Test
     fun `start investigation should say about found human body`() {
-        val investigation = mysteryDeathInvestigation()
-
-        assertThat(investigation.investigate(StartInvestigation(detectiveThomas)))
-            .isEqualTo(
-                CommandResult(
-                    event = InvestigationStarted(detectiveThomas),
-                    storyMessage = "Police is on the crime scene. Neighbour call to you that they have found Harry death body. His apartment is in city center."
-                )
+        mysteryDeathInvestigation()
+            .whenDetective(
+                StartInvestigation(detectiveThomas)
+            ).then(
+                event = InvestigationStarted(detectiveThomas),
+                storyMessage = "Police is on the crime scene. Neighbour call to you that they have found Harry death body. His apartment is in city center."
             )
     }
 
     @Test
     fun `detective can move to victim house`() {
+        mysteryDeathInvestigation(
+            InvestigationStarted(detectiveThomas)
+        ).whenDetective(
+            VisitLocation(detectiveThomas, where = harryHouse.id)
+        ).then(
+            event = DetectiveMoved(detectiveThomas, to = harryHouse.id),
+            storyMessage = "You have visited victims house. Police officer is waiting for you here."
+        )
+    }
+
+    @Test
+    fun `after moved to victim hose, current detective location is harry house`() {
         val investigation = mysteryDeathInvestigation(
-            StartInvestigation(detectiveThomas)
+            InvestigationStarted(detectiveThomas),
+            DetectiveMoved(detectiveThomas, to = harryHouse.id)
         )
 
-        assertThat(investigation.investigate(VisitLocation(detectiveThomas, where = harryHouse.id)))
-            .isEqualTo(
-                CommandResult(
-                    event = DetectiveMoved(detectiveThomas, to = harryHouse.id),
-                    storyMessage = "You have visited victims house. Police officer is waiting for you here."
-                )
-            )
         assertThat(investigation.detectiveLocation()).isEqualTo(harryHouse.id)
     }
 
     @Test
     fun `detective can search crime scene at victim house`() {
-        val investigation = mysteryDeathInvestigation(
+        mysteryDeathInvestigation(
             InvestigationStarted(detectiveThomas),
             DetectiveMoved(detectiveThomas, to = harryHouse.id)
-        )
-
-        assertThat(
-            investigation.investigate(
-                SearchCrimeScene(detectiveThomas, at = harryHouseId)
-            )
-        ).isEqualTo(
-            CommandResult(
-                event = CrimeSceneSearched(at = harryHouseId, by = detectiveThomas),
-                storyMessage = "You have searched crime scene. Try to secure items."
-            )
+        ).whenDetective(
+            SearchCrimeScene(detectiveThomas, at = harryHouseId)
+        ).then(
+            event = CrimeSceneSearched(at = harryHouseId, by = detectiveThomas),
+            storyMessage = "You have searched crime scene. Try to secure items."
         )
     }
 
     @Test
-    fun `at victim house detective can talk with victim daughter`(){
-        val investigation = mysteryDeathInvestigation(
+    fun `at victim house detective can talk with victim daughter`() {
+        mysteryDeathInvestigation(
             InvestigationStarted(detectiveThomas),
             DetectiveMoved(detectiveThomas, to = harryHouse.id)
-        )
-        assertThat(
-            investigation.investigate(
-                LetsChatWith(ask = alice.first, askedBy = detectiveThomas)
-            )
-        ).isEqualTo(
-            CommandResult.onlyMessage("Alice: I'm really scared! My dad was killed by someone...")
+        ).whenDetective(
+            LetsChatWith(ask = alice.first, askedBy = detectiveThomas)
+        ).then(
+            "Alice: I'm really scared! My dad was killed by someone..."
         )
     }
 
     @Test
     fun `after search crime scene, detective can secure knife`() {
-        val investigation = mysteryDeathInvestigation(
+        mysteryDeathInvestigation(
             InvestigationStarted(detectiveThomas),
             DetectiveMoved(detectiveThomas, to = harryHouse.id),
             CrimeSceneSearched(at = harryHouseId, by = detectiveThomas)
-        )
-
-        assertThat(
-            investigation.investigate(
-                SecureTheEvidence(detectiveThomas, at = harryHouseId, itemId = Knife.id)
-            )
-        ).isEqualTo(
-            CommandResult(
-                event = ItemWasFound(itemId = Knife.id, detectiveId = detectiveThomas),
-                storyMessage = "Item was secured!"
-            )
+        ).whenDetective(
+            SecureTheEvidence(detectiveThomas, at = harryHouseId, itemId = Knife.id)
+        ).then(
+            event = ItemWasFound(itemId = Knife.id, detectiveId = detectiveThomas),
+            storyMessage = "Item was secured!"
         )
     }
 
     @Test
-    fun `when detective found knife, victim daughter tell that knife belongs to her brother`(){
-        val investigation = mysteryDeathInvestigation(
+    fun `when detective found knife, victim daughter tell that knife belongs to her brother`() {
+        mysteryDeathInvestigation(
             InvestigationStarted(detectiveThomas),
             DetectiveMoved(detectiveThomas, to = harryHouse.id),
             CrimeSceneSearched(at = harryHouseId, by = detectiveThomas),
             ItemWasFound(itemId = Knife.id, detectiveId = detectiveThomas)
-        )
-
-        assertThat(
-            investigation.investigate(
-                AskAboutItem(ask = alice.first, askedBy = detectiveThomas, askAbout = Knife.id)
-            )
-        ).isEqualTo(
-            CommandResult.onlyMessage("Alice: Oh! This knife belongs to my brother.")
+        ).whenDetective(
+            AskAboutItem(ask = alice.first, askedBy = detectiveThomas, askAbout = Knife.id)
+        ).then(
+            "Alice: Oh! This knife belongs to my brother."
         )
     }
 
     @Test
-    fun `when detective found knife, lab technician tell that knife has fingerprint of victim daughter`(){
-        val investigation = mysteryDeathInvestigation(
+    fun `when detective found knife, lab technician tell that knife has fingerprint of victim daughter`() {
+        mysteryDeathInvestigation(
             InvestigationStarted(detectiveThomas),
             DetectiveMoved(detectiveThomas, to = harryHouse.id),
             CrimeSceneSearched(at = harryHouseId, by = detectiveThomas),
             ItemWasFound(itemId = Knife.id, detectiveId = detectiveThomas)
-        )
-
-        assertThat(
-            investigation.investigate(
-                AskAboutItem(ask = labTechnicianJohn.first, askedBy = detectiveThomas, askAbout = Knife.id)
-            )
-        ).isEqualTo(
-            CommandResult.onlyMessage("John: On the Knife, I've found fingerprints of Alice - Harry's daughter.")
+        ).whenDetective(
+            AskAboutItem(ask = labTechnicianJohn.first, askedBy = detectiveThomas, askAbout = Knife.id)
+        ).then(
+            "John: On the Knife, I've found fingerprints of Alice - Harry's daughter."
         )
     }
 
 }
+
+private fun Investigation.whenDetective(command: Command): ICommandResult = this.investigate(command)
+
+private fun ICommandResult.then(commandResult: CommandResult) = assertThat(this).isEqualTo(commandResult)
+private fun ICommandResult.then(event: DomainEvent, storyMessage: StoryMessage) =
+    assertThat(this).isEqualTo(CommandResult(event, storyMessage))
+private fun ICommandResult.then(storyMessage: StoryMessage) =
+    assertThat(this).isEqualTo(CommandResult.onlyMessage(storyMessage))
 
 private fun mysteryDeathInvestigation(vararg event: DomainEvent) =
     mysteryDeathInvestigation(listOf(*event), emptyList())
